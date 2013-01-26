9a69f5be97926c8e801c5264de93aa22c458e9e3
Hanson Wu <hansonywu@gmail.com>
2013-01-25 16:25:05 -0800

    Add name field to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130125194852) do
+ActiveRecord::Schema.define(:version => 20130125234702) do
 
   create_table "status_changes", :force => true do |t|
     t.boolean  "in"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20130125194852) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
