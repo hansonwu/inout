fb81980e303a1f7876c30ddf9b92c9bffcad3a7f
Hanson Wu <hansonywu@gmail.com>
2013-01-27 02:38:55 -0800

    Add error message to AJAX

--- app/assets/javascripts/status_changes.js
+++ app/assets/javascripts/status_changes.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
-$('#refresh-board')
-  .bind("ajax:success", function(evt, data, status, xhr) {
-    $('#users-table').html(data.html)
-  });
-});
+ $('#refresh-board').
+ bind("ajax:success", function(evt, data, status, xhr) {
+   $('#users-table').remove();
+       $('#board').append(data.html);
+ }).
+ bind("ajax:error", function(evt, data, status, xhr) {
+   alert("Refresh failed. Please try again.");
+ });
 
-function refreshBoard() {
-  $('#refresh-board').click();
-}
-/* Automatically refresh every 10 seconds */
-window.setInterval(refreshBoard, 10000);
+ function refreshBoard() {
+   $('#refresh-board').click();
+ }
+ 
+ /* Automatically refresh every 10 seconds */
+ window.setInterval(refreshBoard, 10000);
+});
