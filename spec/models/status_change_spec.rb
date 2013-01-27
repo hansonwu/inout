03283443217b99f3160ef4937945fd035fefd2a0
Hanson Wu <hansonywu@gmail.com>
2013-01-27 13:37:52 -0800

    Rspec model tests

--- spec/models/status_change_spec.rb
+++ spec/models/status_change_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe StatusChange do
-  pending "add some examples to (or delete) #{__FILE__}"
+    
+  before do
+    @user = FactoryGirl.create(:user)
+    @status_change = @user.status_changes.build(:in => false, :status => 'Lunch')
+  end
+  
+  subject { @status_change }
+  
+  it { should respond_to(:user_id) }
+  it { should respond_to(:user) }
+  it { should respond_to(:in) }
+  it { should respond_to(:status) }
+  
+  it { should be_valid }
+  
+  its(:user) { should == @user }
+  
+  describe "when user_id is nil" do
+    before { @status_change.user_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when status is too long" do
+    before { @status_change.status = "abc" * 100 }
+    it { should_not be_valid }
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before do
+    @user = FactoryGirl.create(:user)
+  end
+  
+  subject { @user }
+  
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  
+  it { should respond_to(:current_status) }
+  it { should respond_to(:status_changes) }
+  
+  it { should be_valid }
+  
+  describe "status changes" do
+    before { @user.save }
+    let!(:older) do
+      FactoryGirl.create(:status_change, user: @user, created_at: 2.day.ago)
+    end
+    let!(:newer) do
+      FactoryGirl.create(:status_change, user: @user, created_at: 1.hour.ago)
+    end
+    
+    it "should have status changes in correct order" do
+      @user.status_changes.should == [newer, older]
+    end    
+    
+    it "should return newest status as current status" do
+      @user.current_status.should == newer
+    end
+  end
+
 end
